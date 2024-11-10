88881a9bbe0293a39b32c34105fc7282b36e3c05
TuborShu <guirui1111@qq.com>
2024-11-10 21:45:04 +0800

    first commit

---
+++ src/main/java/com/guirui/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.guirui.git;
+
+/**
+ * ClassName: GitTest
+ * Package: com.guirui.git
+ * Description:
+ *
+ * @Author guirui
+ * @Create 2024/11/10 21:41
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
