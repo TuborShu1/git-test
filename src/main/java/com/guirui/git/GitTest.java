0856f2922a6cacb6da4d3a0e00857d1edd464862
TuborShu1 <guirui1111@qq.com>
2024-11-10 22:19:32 +0800

    pull test

--- src/main/java/com/guirui/git/GitTest.java
+++ src/main/java/com/guirui/git/GitTest.java
@@ -18,5 +18,6 @@ public class GitTest {
         System.out.println("master conflict");
         System.out.println("conflig");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
